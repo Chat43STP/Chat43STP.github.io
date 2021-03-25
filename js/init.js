f6636de4e0596f2efd9fbf3ff8c37b51d4c92c50
Chat43STP <81387892+Chat43STP@users.noreply.github.com>
2021-03-25 14:43:17 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,1 +1,10 @@
-
+//@ts-ignore
+firebase.initializeAppp({
+    apiKey: "AIzaSyDHBbmea98s72oGCPLY0wHXubrJ4vWgl1U",
+    authDomain: "chat-43tps.firebaseapp.com",
+    projectId: "chat-43tps",
+    storageBucket: "chat-43tps.appspot.com",
+    messagingSenderId: "498905353473",
+    appId: "1:498905353473:web:3dea53c876c8c8920adf00",
+    measurementId: "G-EZB3LG45EJ"
+});
