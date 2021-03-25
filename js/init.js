51d3d15832cb5cf8ddb9d8dfec17835849fdcda1
Chat43STP <81387892+Chat43STP@users.noreply.github.com>
2021-03-25 15:53:34 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,10 +1,12 @@
-//@ts-ignore
-firebase.initializeAppp({
-    apiKey: "AIzaSyDHBbmea98s72oGCPLY0wHXubrJ4vWgl1U",
-    authDomain: "chat-43tps.firebaseapp.com",
-    projectId: "chat-43tps",
-    storageBucket: "chat-43tps.appspot.com",
-    messagingSenderId: "498905353473",
-    appId: "1:498905353473:web:3dea53c876c8c8920adf00",
-    measurementId: "G-EZB3LG45EJ"
-});
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+   apiKey: "AIzaSyAdLmJJe5p2ufEMQwwTLGg4zUEo235naj4",
+    authDomain: "chatkus1.firebaseapp.com",
+    projectId: "chatkus1",
+    storageBucket: "chatkus1.appspot.com",
+    messagingSenderId: "555426460233",
+    appId: "1:555426460233:web:f295482ed3204e5e174598",
+    measurementId: "G-55EEES53YM"
+    });
